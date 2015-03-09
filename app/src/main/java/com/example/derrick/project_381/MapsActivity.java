62a059b56f26449857b301ef4b84e61db8094fcb
Project381 <daj113@mail.usask.ca>
2015-03-09 11:43:29 -0600

    Added marker changes and markrrOnClick
    On click only shows a basic sysout
    the markers are not dynamic

--- app/src/main/java/com/example/derrick/project_381/MapsActivity.java
+++ app/src/main/java/com/example/derrick/project_381/MapsActivity.java
@@ -11,13 +11,28 @@ package com.example.derrick.project_381;
    Project_381
  */
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -28,7 +43,20 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                System.out.println("Clicked Marker! : " + marker.getId() + " : " + marker.getPosition() + " : " + marker.getSnippet());
+                return false;
+            }
+        });
+
     }
+    public boolean handleMarkerClick(){
+        return true;
+    }
+
 
     @Override
     protected void onResume() {
@@ -71,6 +99,87 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        // Enable MyLocation Layer of Google Map
+        mMap.setMyLocationEnabled(true);
+
+        // Get LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Get Current Location
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        // Set map type
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // Get latitude of current location needs phone and GPS
+        //double latitude = myLocation.getLatitude();
+        double latitude = 52.293121;
+
+
+        // Get longitude of the current location needs phone and GPS
+        //double longitude = myLocation.getLongitude();
+        double longitude = -106.657838;
+        // Create a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        // Set zoom in google maps
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Test markers
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title("You are here!")
+                .snippet("Your current location on google maps"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(22.222855, -159.496132))
+                .title("Pu'u Poa Condo")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.photo4))
+                        //.icon(BitmapDescriptorFactory.fromBitmap(bm))
+                .snippet("Population: 776733")
+                .flat(false));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(52.133214, -106.670046))
+                .title("Saskatoon")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.photo3))
+                        //.icon(BitmapDescriptorFactory.fromBitmap(bm))
+                .snippet("Mountains"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(22.220611, -159.583334))
+                .title("Ke'e")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.photo4))
+                        //.icon(BitmapDescriptorFactory.fromBitmap(bm))
+                .snippet("Waves"));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(52.318366, -106.575075))
+                .title("Warman")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.photo5))
+                        //.icon(BitmapDescriptorFactory.fromBitmap(bm))
+                .snippet("Foot Prints"));
     }
 }
+/*
+    private void loadLocations() {
+        dataSource.open();
+        ArrayList <LocationEntry> locations = dataSource.getLocationsFromTrack(trackId);
+        dataSource.close();
+        for(int i=0; i<locations.size(); i++)
+        {
+            map.addMarker(new MarkerOptions()
+                    .position(locations.get(i).getCoordinates())
+                    .title(locations.get(i).getName())
+                    .snippet(locations.get(i).getSnippet())
+                    .icon(BitmapDescriptorFactory.fromPath(locations.get(i).getPhotoURL()))); //here
+        }
+    }*/
